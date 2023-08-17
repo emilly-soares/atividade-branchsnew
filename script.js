9fcda2564a21458b36c689a93ae4bb5017e44741
Emilly Soares Santos <emilly.santos@novaandradina.org>
2023-08-16 21:09:26 -0400

    update script.js

--- script.js
+++ script.js
@@ -5,23 +5,3 @@ function addToDisplay(value) {
    updateDisplay();
 }
 
-function updateDisplay() {
-   document.getElementById("display").value = displayValue;
-}
-
-function calculate() {
-   try {
-      displayValue = eval(displayValue);
-      updateDisplay();
-   } catch (error) {
-      displayValue = "Erro";
-      updateDisplay();
-   }
-}
-
-function clearDisplay() {
-   displayValue = "";
-   updateDisplay();
-}
-
-window.onload = updateDisplay;
