691d0388c06d5fa70b733635602a7844e04c287c
Emilly Soares Santos <emilly.santos@novaandradina.org>
2023-08-16 21:07:18 -0400

    insert index

---
+++ script.js
@@ -0,0 +1,27 @@
+let displayValue = "";
+
+function addToDisplay(value) {
+   displayValue += value;
+   updateDisplay();
+}
+
+function updateDisplay() {
+   document.getElementById("display").value = displayValue;
+}
+
+function calculate() {
+   try {
+      displayValue = eval(displayValue);
+      updateDisplay();
+   } catch (error) {
+      displayValue = "Erro";
+      updateDisplay();
+   }
+}
+
+function clearDisplay() {
+   displayValue = "";
+   updateDisplay();
+}
+
+window.onload = updateDisplay;
